deda28f46635549de772f0922a560dd1c6144ff9
Subhrokamal Mukherjee <mukherjeesubhrokamal@gmail.com>
2026-01-16 18:19:25 +0530

    Delete src/logs directory

--- src/logs/mistral_chatbot.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-sentences = [ 
-    "1. build road"
-    "2. build hospital"
-]  # from LLM
-embeddings = model.encode(sentences)
-print(embeddings)
